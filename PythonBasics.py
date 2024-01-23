0e7f7bd0f03b3a0a412d6a943d7bfadeb9c5aa93
I-am-ashok <mashok4102@gmail.com>
2024-01-23 23:08:21 +0530

    added few more scripts

--- PythonBasics.py
+++ PythonBasics.py
@@ -436,4 +436,94 @@ def perform_arithmetic_operations():
     else:
         print("Thank you, closing the application!!")
 
-#perform_arithmetic_operations()
+# perform_arithmetic_operations()
+
+
+from random import shuffle
+
+def Random():
+    for c in range(100):
+       yield ascii(c) 
+    
+#shu=list(Random())
+#print(shu)
+
+import re
+def remodule():
+    re_split=re.split(" ","ashokkumar 124ashok123 457_ashok")
+    re_sub=re.sub("k","#","ashokkumar 124ashok123457 ashok")
+    re_subn=re.subn("k","#","ashokkumar 124ashok123457 ashok")
+    print(re_split)
+    print(re_sub)
+    print(re_subn)
+
+list=[]
+def listoper():
+    list.append("Ashok")
+    list.insert(1,34)
+    list.extend("B.Tech")    
+    print(list)
+    
+#listoper()
+
+# write a program to check the number is prime/odd/even ?
+
+def checknum():
+    n=int(input("Enter the number to check  either prime/Odd/Even? ==>"))
+    if n==1:
+        return "it is Odd and not prime"
+    elif n%2==0:
+        return "Num is prime and it is Even" if n==2 else "Num is even and not prime"
+    elif n%2!=0:
+        for l in range(2,n):
+            if n%l==0:
+                return "Num is not Prime but it is odd"
+            
+    return "Num is prime and it is also odd"
+
+def recursive():
+    print(checknum())
+    useroption=input("Do you want to continue? Y\\N ")
+    if useroption=='Y':
+        while useroption=='Y':
+            print(checknum())
+            useroption=input("Do you want to continue? Y\\N ")
+    else:
+        pass
+    return "closing the application!!"
+
+#print(recursive())
+
+def printprimenum():
+    primes=[]
+    n=int(input("Prime numbers you want to print till: "))
+    def isprime(n):
+        if n<2:
+            return True
+        for p in range(2,int(n**0.5)+1):
+            if n%p==0:
+                return False
+        return True
+        
+    for p in range(2,n+1):
+        if isprime(p):
+            print(p,end=" ")
+
+#printprimenum()
+
+def febanacci(n):
+    a,b=0,1
+    for _ in range(n):
+     yield a
+     a,b=b,a+b   
+result=[]
+result.extend(febanacci(10))
+#print(result)
+
+# use of Assert
+
+def division(a,b):
+    assert b!=0
+    return a/b
+#print(f"The Assert value is {division(10,0)}")
+
